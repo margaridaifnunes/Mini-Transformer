96d4f28032fb3fa3feade6ef44c9398bfe012022
Margarida Nunes <mifnunes2007@gmail.com>
2026-04-27 09:24:44 +0100

    Update dots.s
    Nova Versão do enunciado V1.1 (falta somente a questão do overflow)

--- dots.s
+++ dots.s
@@ -7,86 +7,87 @@
 # *    - Victória Bernaz                                 ISTID: ist1117771
 # *
 # *********************************************************************
-
+# You can change these values to test your solution.
 .data
-# You can change these arrays to test other values
-A: .word -3, 2, -1, 7, -2   # Initial array A values				 
-B: .word 6, 1, -1, 3, 2   # Initial array B values
+A:    .word 6, 1, 3, 9, 12, 4, 13, 153
+B:    .word 6, 1, 3, 9, 12, 4, 13, 153
+SIZE: .word 8
 
 .text
 
-# **********************************************************************
+# ************************
 # * Constantes
-# **********************************************************************
+# ************************
 
 # Constantes numericas:
 .equ CINQUENTA, 50
+.equ ZERO, 0
+.equ DUZENTOS, 200
 
 main:
-  la a0, A          # a0 = pointer to array A
-  la a1, B          # a1 = pointer to array B
-  li a2, 5          # a2 = number of elements in each array
+  la a1, A          # a1 = pointer to array A
+  la a2, B          # a2 = pointer to array B
+  lw a3, SIZE       # a3 = number of elements in each array
 
-  ble a2, zero, code50    # a2 <= 0; tem tamanho invalido: Call code50 function
-
-  jal ra, dot      # Call dot function
-
-  # Result: a0 contains the dot product of the two integer arrays
+  jal ra, dot                  # call dot function
 
 exit:
-  li a7, 10              # Exit syscall code
-  ecall                  # Terminate the program
+  li a7, 10         # exit syscall code
+  ecall             # terminate the program
 
 
 # ==========================================================================
 # FUNCTION: dot
 #   This function computes the dot product of two integer arrays.
 # Arguments:
-#   a0 = pointer to first array
-#   a1 = pointer to second array
-#   a2 = array length
+#   a1 = pointer to first array
+#   a2 = pointer to second array
+#   a3 = array length
 # Returns:
-#   a0 = dot product result
-# Exceptions:
-#   - If the length of the array is less than 1,
-#     this function terminates the program with error code 50
+#   a0 = status code
+#   a1 = dot product result
 # ===========================================================================
-
 dot:
+  # TODO: Implement the dot product function here
   li t0,0                     # initializing t0 (index dos arrays) to 0
   li t1,0                     # initializing t1 (final_result) to 0
   li t4,0                     # initializing t4 (term result) to 0
 
+  ble a3, zero, code50
 
 dot_loop:
-  bge t0, a2, loop_end    # t0 >= a2 exit
 
-  lw t2,0(a0)                 # t2 = current index value
-  lw t3,0(a1)                 # t3 = current index value
+  bge t0, a3, loop_end    # t0 >= a2 exit
+
+  lw t2,0(a1)                 # t2 = current index value
+  lw t3,0(a2)                 # t3 = current index value
 
   mul t4, t2, t3    # t4 = t2 * t3 (term)
   add t1, t1, t4    # current result
 
-  addi a0,a0,4                  # next value
   addi a1,a1,4                  # next value
+  addi a2,a2,4                  # next value
 
   addi t0, t0, 1    # incrementar o indice do array
   j dot_loop
 
-
 loop_end:
-  add a0,t1,zero
-  jr ra                  # normal return
+  add a1,t1,zero
+  j code0
+
+dot_end:
+  jr ra               # return to the caller
 
 
-# Exits the program with an error 
-# Arguments: 
-# a0 (int) is the error code 
-# You need to load a0 the error to a0 before to jump here
-exit_with_error:
-  li a7, 93            # Exit system call
-  ecall                # Terminate program
 
-code50:
-  li a0,CINQUENTA              # a0 = 50 (error code)
-  j exit_with_error            # call exit_with_error function
+code0:  # sucess
+  li a0, ZERO # a0 = 0
+  j dot_end
+
+code50: # invalid argument
+  li a0,CINQUENTA   # a0 = 50
+  j dot_end
+
+code200:  # detected overflow
+  li a0, DUZENTOS  # a0 = 200
+  j dot_endth_error function
