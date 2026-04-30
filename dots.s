35c97b0c7bfacdefde45ee20eae756b08bd9863b
Margarida Nunes <mifnunes2007@gmail.com>
2026-04-30 13:23:57 +0100

    Update dots.s

--- dots.s
+++ dots.s
@@ -14,12 +14,9 @@ B:    .word 6, 1, 3, 9, 12, 4, 13, 153
 SIZE: .word 8
 
 .text
-
-# ************************
-# * Constantes
-# ************************
-
-# Constantes numericas:
+# ==========================================================================
+# Constantes numéricas:
+# ==========================================================================
 .equ CINQUENTA, 50
 .equ ZERO, 0
 .equ DUZENTOS, 200
@@ -29,7 +26,7 @@ main:
   la a2, B          # a2 = pointer to array B
   lw a3, SIZE       # a3 = number of elements in each array
 
-  jal ra, dot                  # call dot function
+  jal ra, dot       # call dot function
 
 exit:
   li a7, 10         # exit syscall code
@@ -49,7 +46,7 @@ exit:
 # ===========================================================================
 dot:
 
-  addi sp, sp, -4  # sp - stack pointer
+  addi sp, sp, -4             # sp - stack pointer
   sw ra, 0(sp)
   
   li t0,0                     # initializing t0 (index dos arrays) to 0
@@ -59,52 +56,58 @@ dot:
   ble a3, zero, code50        # validation of array size limit
 
 dot_loop:
-  bge t0, a3, loop_end    # t0 >= a2 exit
+  bge t0, a3, loop_end        # t0 >= a2 exit
 
   lw t2,0(a1)                 # t2 = current index value
   lw t3,0(a2)                 # t3 = current index value
 
-  mul t4, t2, t3    # t4 = t2 * t3 (term)
-  jal ra, overflow_mul
-  jal ra, overflow_sum
-  # na soma com sinais diferentes nunca à overflow; se tiverem sinais iguais é preciso fazer a conta e verificar
-  addi a1,a1,4      # next value
-  addi a2,a2,4      # next value
+  mul t4, t2, t3              # t4 = t2 * t3 (term)
+  jal ra, overflow_mul        # checking overflow(mult)
+  jal ra, overflow_sum        # checking overflow(sum) and executing sum
 
-  addi t0, t0, 1    # incrementar o indice do array
+  addi a1,a1,4                # next value
+  addi a2,a2,4                # next value
+
+  addi t0, t0, 1              # increment the index by 1
   j dot_loop
 
 
 loop_end:
-  add a1,t1,zero  # a1 = final result of the operation
-  j code0         # sign of sucess
+  add a1,t1,zero    # a1 = final result of the operation
+  j code0           # sign of sucess
 
 dot_end:
-  lw ra, 0(sp)      # restaurar ra original
-  addi sp, sp, 4    # restaurar sp
-  jr ra               # return to the caller
+  lw ra, 0(sp)      # reinstate original ra
+  addi sp, sp, 4    # reinstate sp
+  jr ra             # return to the caller
 
+
+# ==========================================================================
+# Overflow Check:
+# ==========================================================================
 overflow_mul:
-  mulh t5,t2,t3      # 32 bits altos em t5                     #### editar comments daqui para baixo
-  srai t6,t4,31        # insere o bit mais significativo à esq.
+  mulh t5,t2,t3        # the upper 32 bits in t5
+  srai t6,t4,31        # inserting MSB on the left
   
-  bne t5,t6,code200  # se os sinais forem diferentes... o sinal mudou e houve overflow
+  bne t5,t6,code200    # if t5 and t6 signals are diferentes - overflow detected
   jr ra
   
-overflow_sum:            ####### REVER
-  srli t5,t1,31      # sinal do acumulado (t1)
-  srli t6,t4,31      # sinal do termo atual (t4)
-  xor t5,t5,t6      # dá 0 se forem bits iguais e 1 se forem bits diferentes
+overflow_sum
+  srli t5,t1,31        # cumulative signal (t1)
+  srli t6,t4,31        # current term signal (t4)
+  xor t5,t5,t6         # 0 -> equal bits; 1 -> diferent bits
 
-  add t1, t1, t4    # current result (sum tresult)
-  srli t6,t1, 31    # reutilizar t6
+  add t1, t1, t4       # current result (sum result)
+  srli t6,t1, 31       # reuse t6
 
-  # compara sinal do resultado com sinal original
-  bne t6,zero,code200
+  bne t6,zero,code200  # sign comparison between t6 and zero
   jr ra
-  
+
+# ==========================================================================
+# Codes (0,50,200):
+# ==========================================================================
 code0:  # sucess
-  li a0, ZERO  # a0 = 0
+  li a0, ZERO       # a0 = 0
   j dot_end
 
 code50: # invalid argument
