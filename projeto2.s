a63db72b7c670eed8d78ebf6aa619a7be95450d6
Victória Bernaz <bernazvictoria07@gmail.com>
2026-05-14 13:14:27 +0100

    Update projeto2.s

--- projeto2.s
+++ projeto2.s
@@ -1,3 +1,13 @@
+# ===========================================================================
+# PROJETO_2 IAC
+# Grupo: Número 18
+# Grupo constituído por:
+#    - Henrique Ascenção Lopes da Silva Gonçalves      ISTID: ist1109966
+#    - Margarida Isabel Farinha Nunes                  ISTID: ist1117809
+#    - Victória Bernaz                                 ISTID: ist1117771
+# 
+# ===========================================================================
+
 ###########################################################################
 # Upper bound constants for static memory reservation
 ###########################################################################
