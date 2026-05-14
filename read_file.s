86d7570fac35de78fda9d063393002689cc1cb97
Margarida Nunes <mifnunes2007@gmail.com>
2026-05-14 13:41:51 +0100

    Create read_file.s

---
+++ read_file.s
@@ -0,0 +1,45 @@
+# FUNCTION READ_FILE:
+
+# Read from a text file into a buffer. 
+# (in)  a0: filename address (char*) 
+# (in/out) a1: destination buffer 
+# (in)     a2: maximum number of bytes to read 
+read_file: 
+
+addi sp,sp,-16
+sw ra, 12(sp)
+sw a1, 8(sp)
+sw a2, 4(sp)
+
+# verificar tamanho do buffer se a2 < 1 não vale a pena ler erro 42?
+
+# Abrir o ficheiro:
+li a1, 0 # flag a zero 
+li a7, 1024  # open
+ecall    # a0 = fd (fd < 0 se houve erro)
+
+# verificar se fd é válido (fd >= 0):
+blt a0, x0, invalid_fd  # fd < 0 (error 41)
+sw a0, 0(sp)  # guardar fd na stack para depois fazer close
+
+# Ler o ficheiro:
+lw a1, 8(sp)  # restaurar o endereço do buffer
+lw a2, 4(sp)  # restaurar o tamanho
+li a7, 63     # ler o ficheiro
+ecall
+
+### FALTA
+# Fechar o ficheiro:
+lw a0, 0(sp)
+li a7, 57
+ecall
+
+# Restaurar a stack:
+lw ra, 12(sp)
+addi sp, sp, 16
+jr ra
+
+invalid_fd:
+  li ao, 42
+  li a7, 93
+  ecall
