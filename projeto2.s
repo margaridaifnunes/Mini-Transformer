14b7fbbd1de0f5f5928ce9fd25cc40cd552ed470
Victória Bernaz <bernazvictoria07@gmail.com>
2026-05-14 13:51:56 +0100

    read_vocab_comments

--- projeto2.s
+++ projeto2.s
@@ -75,20 +75,22 @@ main:
     ###########################################################################
     # Read vocabulary
     ###########################################################################
-    la a0, VOCABULARY_FILENAME
+    la a0, VOCABULARY_FILENAME  # colocao adress o ficheiro do vocabulário
     li a1, 0
     li a2, 0
-    li a7, CONST_SYSCALL_OPEN
+    li a7, CONST_SYSCALL_OPEN   # escolhe syscall open
+    ecall                       # executa syscall (fd) fica em a0
+    mv s0, a0                   # guarda fd depois do ecall open
 
-    mv a0, s0
-    la a1, VOCAB_BUFFER
-    li a2, CONST_BUFFER_SIZE
-    li a7, CONST_SYSCALL_READ
-    ecall 
+    mv a0, s0                   # fd fica em a0 antes de read
+    la a1, VOCAB_BUFFER         # coloca o adress onde vai colocar o buffer do ficheiro
+    li a2, CONST_BUFFER_SIZE    # coloca em a2 o número máximo de bytes a ler
+    li a7, CONST_SYSCALL_READ   # escolhe syscall read
+    ecall                       # executa leitura
 
-    mv a0, s0
-    li a7, CONST_SYSCALL_CLOSE
-    ecall
+    mv a0, s0                   # coloca o fd em a0 para o close
+    li a7, CONST_SYSCALL_CLOSE  # escolhe syscall close
+    ecall                       # close file
 
     ###########################################################################
     # Read input
