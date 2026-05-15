f028083c273fe1e7eec1afe3d4bbfe284bef36bf
Henrispyky <henrispykyganeto@gmail.com>
2026-05-15 18:52:18 +0100

    Update projeto2.s
    my numba :)

--- projeto2.s
+++ projeto2.s
@@ -2,7 +2,7 @@
 # PROJETO_2 IAC
 # Grupo: Número 18
 # Grupo constituído por:
-#    - Henrique Ascenção Lopes da Silva Gonçalves      ISTID: ist1109966
+#    - Henrique Ascenção Lopes da Silva Gonçalves      ISTID: ist1118062
 #    - Margarida Isabel Farinha Nunes                  ISTID: ist1117809
 #    - Victória Bernaz                                 ISTID: ist1117771
 # 
